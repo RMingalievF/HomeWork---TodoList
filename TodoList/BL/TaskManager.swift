461728f39b483be02d9658fca3870a41f10226f5
Ruslan <RMingalievF@icloud.com>
2023-10-19 12:46:29 +0500

    Initial Commit

---
+++ TodoList/BL/TaskManager.swift
@@ -0,0 +1,8 @@
+//
+//  TaskManager.swift
+//  TodoList
+//
+//  Created by Руслан Мингалиев on 19.10.2023.
+//
+
+import Foundation
---
+++ TodoList/BL/Tasks.swift
@@ -0,0 +1,8 @@
+//
+//  Tasks.swift
+//  TodoList
+//
+//  Created by Руслан Мингалиев on 19.10.2023.
+//
+
+import Foundation
