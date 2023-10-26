ca9176729cdf00dfb5cc66020873e3ba52de0d84
Ruslan <RMingalievF@icloud.com>
2023-10-26 12:49:49 +0300

    fix: class Tasks, add var date

--- TodoList/BL/Tasks.swift
+++ TodoList/BL/Tasks.swift
@@ -17,28 +17,30 @@ class Tasks {
 	}
 }
 
-class RegularTask: Tasks {
+final class RegularTask: Tasks {
 }
 
-class ImportantTask: Tasks {
+final class ImportantTask: Tasks {
 	enum TaskPriority {
 		case high
 		case medium
 		case low
 	}
 	let taskPriority: TaskPriority
+	var date: Date
 	var deadLine: Date? {
 		switch taskPriority {
 		case .high:
-			return Calendar.current.date(byAdding: .day, value: 1, to: Date())
+			return Calendar.current.date(byAdding: .day, value: 1, to: date)
 		case .medium:
-			return Calendar.current.date(byAdding: .day, value: 2, to: Date())
+			return Calendar.current.date(byAdding: .day, value: 2, to: date)
 		case .low:
-			return Calendar.current.date(byAdding: .day, value: 3, to: Date())
+			return Calendar.current.date(byAdding: .day, value: 3, to: date)
 		}
 	}
-	init(title: String, taskPriority: TaskPriority) {
+	init(title: String, date: Date, taskPriority: TaskPriority) {
 		self.taskPriority = taskPriority
+		self.date = date
 		super.init(title: title, completed: false)
 	}
 }
