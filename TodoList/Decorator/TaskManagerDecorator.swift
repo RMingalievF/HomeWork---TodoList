47ed6905e520c2d476c8621194d5f950d9d596a9
Ruslan <RMingalievF@icloud.com>
2023-10-27 00:00:32 +0300

    improvement: add comments

--- TodoList/Decorator/TaskManagerDecorator.swift
+++ TodoList/Decorator/TaskManagerDecorator.swift
@@ -5,6 +5,8 @@
 //  Created by Руслан Мингалиев on 26.10.2023.
 //
 
+// NOTE: let importantTask = task as? ImportantTask теперь в importantTask будет доступно свойство priority
+
 import Foundation
 
 class TaskManagerDecorator: ITaskManager {
@@ -18,7 +20,7 @@ class TaskManagerDecorator: ITaskManager {
 		decoratorTasks.addTasks(tasks: tasks )
 	}
 	func getAllTasks() -> [Tasks] {
-		decoratorTasks.getAllTasks().sorted(by: { $0.title > $1.title})
+		decoratorTasks.getAllTasks().sorted(by: { $0.title > $1.title}) // для проверки, временно
 	}
 	func completedTask() -> [Tasks] {
 		decoratorTasks.completedTask()
